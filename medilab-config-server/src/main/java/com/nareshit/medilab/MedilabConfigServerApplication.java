977d084432cbb3e5b043d28c9cdbf9cf99bdd594
Narsi <narsi.sanda@gmail.com>
2021-02-25 09:02:00 +0530

    added the spring cloud bus

--- medilab-config-server/src/main/java/com/nareshit/medilab/MedilabConfigServerApplication.java
+++ medilab-config-server/src/main/java/com/nareshit/medilab/MedilabConfigServerApplication.java
@@ -3,10 +3,12 @@ package com.nareshit.medilab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 
 @EnableConfigServer
 @SpringBootApplication
+@RefreshScope
 public class MedilabConfigServerApplication {
 
 	public static void main(String[] args) {
